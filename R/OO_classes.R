d71d5c73af6889cc98587e791db3161be78d8ce5
Graeme Walsh <graeme.walsh@hotmail.co.uk>
2024-09-26 11:39:07 +0100

    initial commit

---
+++ R/OO_classes.R
@@ -0,0 +1,37 @@
+#' Class definition for statbanktools series-object
+#'
+#' @slot mnemonic character.
+#' @slot description character.
+#' @slot units character.
+#' @slot source character.
+#' @slot table character.
+#' @slot index ANY.
+#' @slot model_desc character.
+#' @slot frequency numeric.
+#' @slot start_date numeric.
+#' @slot end_date numeric.
+#' @slot raw_data ts.
+#' @slot growth_rates mts.
+#' @slot differences mts.
+#' @slot data mts.
+#'
+#' @return A series-object
+#' @export
+#'
+#' @examples
+#' series_obj <- new("series-obj")
+setClass("series-obj",
+         slots = list(mnemonic     = "character",
+                      description  = "character",
+                      units        = "character",
+                      source       = "character",
+                      table        = "character",
+                      index        = "ANY",
+                      model_desc   = "character",
+                      frequency    = "numeric",
+                      start_date   = "numeric",
+                      end_date     = "numeric",
+                      raw_data     = "ts",
+                      #growth_rates = "mts",
+                      #differences  = "mts",
+                      data         = "mts"))
---
+++ R/get_series.R
@@ -0,0 +1,136 @@
+#' Get series from a CSO table
+#'
+#' @param table_px A px object from statbanker package.
+#' @param index An integer or string to locate the series.
+#' @param ... Other types of arguments.
+#'
+#' @return A series-object.
+#' @export
+#'
+#' @examples
+#' library(statbanker)
+#' library(plotly)
+#' NAQ01 <- getStatBankData("NAQ01", type = "px")
+#' PCR_SA <- get_series(NAQ01, index = 2)
+get_series <- function(table_px, index, ...){
+
+  # Extract the time-series
+  series_ts  <- extract_series(table_px, index)
+
+  if(frequency(series_ts) == 4){
+
+    # Compute the growth rates
+    series_ts_gr <- cbind(compute_growth_rates(series_ts, 1),
+                          compute_growth_rates(series_ts, 4))
+    #colnames(series_ts_gr) <- c("Q-on-Q","Y-on-Y")
+
+    # Compute the differences
+    series_ts_diff <- cbind(compute_differences(series_ts, 1),
+                            compute_differences(series_ts, 4))
+    #colnames(series_ts_diff) <- c("Q-diff", "Y-diff")
+
+  }else if(frequency(series_ts) == 1){
+
+    # Compute the growth rates
+    series_ts_gr <- compute_growth_rates(series_ts, 1)
+    #colnames(series_ts_gr) <- c("Y-on-Y")
+
+    # Compute the differences
+    series_ts_diff <- compute_differences(series_ts, 1)
+    #colnames(series_ts_diff) <- c("Y-diff")
+
+  }else if(frequency(series_ts) == 12){
+
+    # Compute the growth rates
+    series_ts_gr <- cbind(compute_growth_rates(series_ts, 1),
+                          compute_growth_rates(series_ts, 12))
+    #colnames(series_ts_gr) <- c("M-on-M","Y-on-Y")
+
+    # Compute the differences
+    series_ts_diff <- cbind(compute_differences(series_ts, 1),
+                            compute_differences(series_ts, 12))
+    #colnames(series_ts_diff) <- c("M-diff", "Y-diff")
+
+  }
+
+  # Merge data and transformations
+  data_mts <- cbind(series_ts,
+                    series_ts_gr,
+                    series_ts_diff)
+  if(frequency(series_ts) == 4){
+    colnames(data_mts) <- c("level", "Q-on-Q", "Y-on-Y", "Q-diff", "Y-diff")
+  }else if(frequency(series_ts) == 1){
+    colnames(data_mts) <- c("level", "Y-on-Y", "Y-diff")
+  }else if(frequency(series_ts) == 12){
+    colnames(data_mts) <- c("level", "M-on-M", "Y-on-Y", "M-diff", "Y-diff")
+  }
+
+  if(class(index) == "numeric"){
+    desc <- (unlist(strsplit(colnames(px2ts(table_px))[index], '_')))
+  }else if(class(index) == "character"){
+    id   <- which(colnames(px2ts(table_px))==index)
+    desc <- (unlist(strsplit(colnames(px2ts(table_px))[id], '_')))
+  }
+
+  # Package together the data and metadata
+  series_obj <- new("series-obj",
+                    #mnemonic     = mnemonic,
+                    #description  = (unlist(strsplit(colnames(px2ts(table_px))[index], '_'))),
+                    description  = desc,
+                    units        = table_px$UNITS$value,
+                    source       = c(table_px$SOURCE$value,
+                                     table_px$MATRIX$value),
+                    table        = as.character(substitute(table_px)),
+                    index        = index,
+                    #model_desc   = model_desc,
+                    frequency    = frequency(series_ts),
+                    start_date   = start(series_ts),
+                    end_date     = end(series_ts),
+                    raw_data     = series_ts,
+                    #growth_rates = series_ts_gr,
+                    #differences  = series_ts_diff,
+                    data         = data_mts)
+
+  # Plotting
+  if(class(index) == "numeric"){
+    chart_title <- paste0(sub("_", "\n", colnames(px2ts(table_px))[index]))
+  }else if(class(index) == "character"){
+    id <- which(colnames(px2ts(table_px)) == index)
+    chart_title <- paste0(sub("_", "\n", colnames(px2ts(table_px))[id]))
+  }
+
+  if(frequency(series_ts)==4){
+    #fig1 <- plot_ly(as.data.frame(series_obj@data), x = seq(start(series_obj@data)[1],end(series_obj@data)[1],0.25), y = series_obj@data[,"level"], type = 'scatter', mode = 'lines', name = paste0('Level', " (",table_px$UNITS$value,")"))
+    fig1 <- plot_ly(as.data.frame(series_obj@data), x = seq(from=start(series_obj@data)[1],by=0.25,length.out=length(series_obj@data[,1])), y = series_obj@data[,"level"], type = 'scatter', mode = 'lines', name = paste0('Level', " (",table_px$UNITS$value,")"))
+  }
+  if(frequency(series_ts)==1){
+    #fig1 <- plot_ly(as.data.frame(series_obj@data), x = seq(start(series_obj@data)[1],end(series_obj@data)[1],1), y = series_obj@data[,"level"], type = 'scatter', mode = 'lines', name = paste0('Level', " (",table_px$UNITS$value,")"))
+    fig1 <- plot_ly(as.data.frame(series_obj@data), x = seq(from=start(series_obj@data)[1],by=1,length.out=length(series_obj@data[,1])), y = series_obj@data[,"level"], type = 'scatter', mode = 'lines', name = paste0('Level', " (",table_px$UNITS$value,")"))
+  }
+  if(frequency(series_ts)==12){
+    #fig1 <- plot_ly(as.data.frame(series_obj@data), x = seq(start(series_obj@data)[1],end(series_obj@data)[1],1/12), y = series_obj@data[,"level"], type = 'scatter', mode = 'lines', name = paste0('Level', " (",table_px$UNITS$value,")"))
+    fig1 <- plot_ly(as.data.frame(series_obj@data), x = round(seq(from=start(series_obj@data)[1],by=1/12,length.out=length(series_obj@data[,1])),2), y = series_obj@data[,"level"], type = 'scatter', mode = 'lines', name = paste0('Level', " (",table_px$UNITS$value,")"))
+  }
+
+  if(frequency(series_ts)==4){
+    fig1 <- fig1 %>% add_trace(y = series_obj@data[,"Q-diff"], type = 'scatter', mode = 'lines+markers', name = paste0('Q-diff', " (",table_px$UNITS$value,")"), visible='legendonly')
+  }
+  if(frequency(series_ts)==12){
+    fig1 <- fig1 %>% add_trace(y = series_obj@data[,"M-diff"], type = 'scatter', mode = 'lines+markers', name = paste0('M-diff', " (",table_px$UNITS$value,")"), visible='legendonly')
+  }
+
+  fig1 <- fig1 %>% add_trace(y = series_obj@data[,"Y-diff"], type = 'scatter', mode = 'lines+markers', name = paste0('Y-diff', " (",table_px$UNITS$value,")"), visible='legendonly')
+  if(frequency(series_ts)==4){
+    fig1 <- fig1 %>% add_trace(y = round(series_obj@data[,"Q-on-Q"],1), type = 'scatter', mode = 'lines+markers', name = 'Q-on-Q (%)', visible='legendonly')
+  }
+  if(frequency(series_ts)==12){
+    fig1 <- fig1 %>% add_trace(y = round(series_obj@data[,"M-on-M"],1), type = 'scatter', mode = 'lines+markers', name = 'M-on-M (%)', visible='legendonly')
+  }
+
+  fig1 <- fig1 %>% add_trace(y = round(series_obj@data[,"Y-on-Y"],1), type = 'scatter', mode = 'lines+markers', name = 'Y-on-Y (%)', visible='legendonly')
+  fig1 <- fig1 %>% layout(title = list(text = chart_title, xanchor = 'center', yanchor = 'top', font = list(family = "Times New Roman", size = 12, color = "black")))
+  fig1 <- fig1 %>% layout(xaxis = list(tickformat = ",.", hoverformat = ",."))
+  print(fig1)
+
+  return(series_obj)
+}
